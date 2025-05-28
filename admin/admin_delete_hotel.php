f5efb18df5416949f7b7a9ba8fc6b198d943f93b
Mahima-Umak2005 <mahimaumak6868@gmail.com>
2025-05-29 02:50:27 +0530

    Delete admin directory

--- admin/admin_delete_hotel.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once "../includes/config.php";
-if (isset($_GET['id'])) {
-    $id = $_GET['id'];
-    $conn->query("DELETE FROM hotels WHERE id = $id");
-}
-header("Location: admin_manage_hotels.php");
-exit();
-?>
